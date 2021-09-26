944f4ecf17cdbddec8ea541998fbd056bfc41da1
logon pierce <logonpierce@gmail.com>
2021-09-26 01:34:02 -0400

    updated routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -16,11 +16,23 @@ const { Tag, Product, ProductTag } = require('../../models');
     }
   });
 
-
-router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-});
+  router.get('/:id', async (req, res) => {
+    try {
+      const TagData = await Tag.findByPk(req.params.id, {
+        include: [{model: Product}],
+      });
+  
+      if (!CategoryData) {
+        res.status(404).json({message: 'No category found with that id!'});
+        return;
+      }
+      res.status(200).json(CategoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.post('/', (req, res) => {
   // create a new tag
