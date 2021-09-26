b1f01e4ea7a2058057950540332340c81bee911d
logon pierce <logonpierce@gmail.com>
2021-09-26 01:57:06 -0400

    updated server

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/connection.js');
 // import sequelize connection
 
 const app = express();
@@ -8,9 +9,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
+sequelize.sync({ force: false }).then(()=> {
+  app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+  });
 });
