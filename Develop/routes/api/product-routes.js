8da6492f04b57251c594c534395d5594cc88722d
logon pierce <logonpierce@gmail.com>
2021-09-26 01:33:17 -0400

    updated routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,16 +4,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+
+
   // find all products
   // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const ProductData = await Product.findAll({
+      include: [{model: Category}, {model: Tag}],
+    });
+    res.status(200).json(ProductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 // get one product
-router.get('/:id', (req, res) => {
+
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  router.get('/:id', async (req, res) => {
+    try {
+      const ProductData = await product.findByPk(req.params.id, {
+        include: [{ model: Category }],
+      });
+  
+      if (!productData) {
+        res.status(404).json({message: 'No tag found with that id!'});
+        return;
+      }
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 // create new product
 router.post('/', (req, res) => {
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,10 +3,19 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-});
+  router.get('/', async (req, res) => {
+    try {
+      const TagData = await Tag.findAll({
+        include: [{ model: Product }],
+      });
+      res.status(200).json(TagData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
